157b3093f6871d454d5cffda8bc7307c809b28c1
majorAndry <majorandry@yahoo.com>
2021-07-25 21:01:33 +0200

    Adds Iterator and Observer patterns.

---
+++ iterator/index.js
@@ -0,0 +1,113 @@
+class Shop {
+  constructor(menu) {
+    this.menu = menu;
+  }
+
+  printMenu() {
+    // If the menu has more data, print the new data.
+    while (this.menu.hasNext()) {
+      this.menu.next();
+    }
+  }
+}
+
+class Iterator {
+  constructor(menu) {
+    this.menu = menu;
+
+    // Return the appropriate Iterator instance for
+    // the type of the menu to be used as loop.
+    switch (typeof this.menu) {
+      case "string":
+        return new StringIterator(this.menu);
+
+      case "object":
+        if (Array.isArray(this.menu)) {
+          return new ArrayIterator(this.menu);
+        }
+        return new ObjectIterator(this.menu);
+    }
+  }
+}
+
+class ArrayIterator {
+  constructor(menu) {
+    this.menu = menu;
+  }
+
+  hasNext() {
+    // If length is true, continue to iterate over the array.
+    return this.menu.length;
+  }
+
+  next() {
+    // Shift the first value out to be printed.
+    console.log(this.menu.shift());
+
+    // Aside: This will change the original array, but I think is fine 
+    // for this demonstration of my implementation of the iterator pattern.
+  }
+}
+
+class ObjectIterator {
+  constructor(menu) {
+    this.menu = menu;
+    this.index = 0;
+  }
+
+  hasNext() {
+    // Get the value of the object at the current index.
+    // If none, then the iteration will stop.
+    return Object.values(this.menu)[this.index];
+  }
+
+  next() {
+    // Print out the key and the value at the current index.
+    console.log(
+      `${Object.keys(this.menu)[this.index]} : ${
+        Object.values(this.menu)[this.index]
+      }`
+    );
+
+    // Increase the index for the next iteration.
+    this.index++;
+  }
+}
+
+class StringIterator {
+  constructor(menu) {
+    this.menu = menu;
+    // Make an array out of the string by splitting the words
+    // by the comma and remove the white space.
+    this.items = menu.split(",").map((item) => item.trim());
+  }
+
+  hasNext() {
+    // If length is true, continue to iterate over the array.
+    return this.items.length;
+  }
+
+  next() {
+    // Pop out the last entry of the array to be printed.
+    console.log(this.items.pop());
+  }
+}
+
+class Menu {
+  constructor(menu) {
+    this.menu = new Iterator(menu);
+    return this.menu;
+  }
+}
+
+const firstMenu = new Menu(["Item 1", "Item 2", "Item 3", "Item 4"]);
+const secondMenu = new Menu({
+  first: "Object 1",
+  second: "Object 2",
+  third: "Object 3",
+  fourth: "Object 4",
+});
+const thirdMenu = new Menu("Thing One, Thing Two, Thing Three, Thing Four");
+const newShop = new Shop(thirdMenu);
+
+newShop.printMenu();
---
+++ observer/index.js
@@ -0,0 +1,76 @@
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn);
+  }
+
+  unsubscribe(fn) {
+    // Filter out the observer to remove.
+    this.observers = this.observers.filter((observer) => observer !== fn);
+  }
+
+  update(data = null) {
+    // Call each observer with the new data.
+    this.observers.forEach((observer) => {
+      observer(data);
+    });
+  }
+}
+
+class WeatherApp {
+  constructor({ temp, wind, uv }) {
+    this.temp = temp;
+    this.wind = wind;
+    this.uv = uv;
+    // Create a new instance of a Subject.
+    this.subject = new Subject();
+  }
+
+  // Subscribe an observer to be called whenever the weather 
+  // data change when using the class method 'updateWeather'.
+  // This method will also return a function to unsubscribe the observer.
+  onWeatherDataChange(fn) {
+    this.subject.subscribe(fn);
+    return () => this.subject.unsubscribe(fn);
+  }
+
+  // Changes the weather data and notify all the observers
+  // subscribed with the class method 'onWeatherDataChange'.
+  updateWeather({ temp, wind, uv }) {
+    this.temp = temp;
+    this.wind = wind;
+    this.uv = uv;
+    // Notify observers with the new data.
+    this.subject.update({ temp: this.temp, wind: this.wind, uv: this.uv });
+  }
+}
+
+const newWeatherApp = new WeatherApp({ temp: 32, wind: 8, uv: "High" });
+
+const reportWeatherCondition = ({ temp, wind, uv }) => {
+  return console.log(`
+    @ Weather Display Type 1
+
+    Temperature: ${temp}
+    Wind: ${wind} km/h
+    UV: ${uv}
+  `);
+}
+
+const anotherReportForWeatherCondition = ({ temp, wind, uv }) => {
+  return console.log(`
+    @ Weather Display Type 2
+
+    Wind: ${wind} km/h
+    UV: ${uv}
+    Temperature: ${temp}
+  `);
+}
+
+newWeatherApp.onWeatherDataChange(reportWeatherCondition);
+newWeatherApp.onWeatherDataChange(anotherReportForWeatherCondition);
+
+newWeatherApp.updateWeather({ temp: 37, wind: 6, uv: "Very High" });
